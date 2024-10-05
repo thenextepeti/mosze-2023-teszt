bd205b9afa0fb730e8f8b4712b36b61142d6a75c
thenextepeti <elekpeti15@gmail.com>
2024-10-05 15:55:58 +0200

    hibák kijelölése

--- main.cpp
+++ main.cpp
@@ -5,22 +5,29 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[NELEMENTS];
+    //' ' helyet " " kell hasznalni nincs ; a sor vegen
     std::cout << '1-100 ertekek duplazasa'
+    //for ciklus iterációja nincs helyesen megadva
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
+    //ez a for ciklus sem helyes
     for (int i = 0; i; i++)
     {
+        sor végén;
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
+    //atlagnak kellene egy kiinduló érték
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+        //sor végén ;
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // memóriafelszabadítás a végén...
     return 0;
 }
